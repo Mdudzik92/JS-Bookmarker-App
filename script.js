ff11f5329ae884c487ed3935d1f029eb16c0b90a
Matt Dudzik <mdudzik92@gmail.com>
2021-07-26 03:51:55 -0500

    Form validation added with regular expression to alert the user to enter a valid url

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Listen for form submit
 document.getElementById("myForm").addEventListener("submit", saveBookmark);
 
-// Save bookmark
+// Save Bookmark
 function saveBookmark(e) {
 	// Get form values
 	var siteName = document.getElementById("siteName").value;
 	var siteUrl = document.getElementById("siteUrl").value;
+
+	if (!validateForm(siteName, siteUrl)) {
+		return false;
+	}
 
 	var bookmark = {
 		name: siteName,
@@ -18,16 +22,19 @@ function saveBookmark(e) {
 		var bookmarks = [];
 		// Add to array
 		bookmarks.push(bookmark);
-		// Set to localStorage with setItem and JSON stringify.
+		// Set to localStorage
 		localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 	} else {
-		// Get (fetch) bookmarks from localStorage with getItem and JSON.parse which turns it back into JSON
+		// Get bookmarks from localStorage
 		var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
 		// Add bookmark to array
 		bookmarks.push(bookmark);
 		// Re-set back to localStorage
 		localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 	}
+
+	// Clear form
+	document.getElementById("myForm").reset();
 
 	// Re-fetch bookmarks
 	fetchBookmarks();
@@ -40,7 +47,7 @@ function saveBookmark(e) {
 function deleteBookmark(url) {
 	// Get bookmarks from localStorage
 	var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
-	// Loop through bookmarks
+	// Loop through the bookmarks
 	for (var i = 0; i < bookmarks.length; i++) {
 		if (bookmarks[i].url == url) {
 			// Remove from array
@@ -71,7 +78,7 @@ function fetchBookmarks() {
 			'<div class="well bg-light">' +
 			"<h3>" +
 			name +
-			' <a type="button" class="btn btn-primary" target="_blank" href="' +
+			' <a class="btn btn-primary" target="_blank" href="' +
 			addhttp(url) +
 			'">Visit</a> ' +
 			" <a onclick=\"deleteBookmark('" +
@@ -82,6 +89,25 @@ function fetchBookmarks() {
 	}
 }
 fetchBookmarks();
+
+// Validate Form
+function validateForm(siteName, siteUrl) {
+	if (!siteName || !siteUrl) {
+		alert("Please fill in the form");
+		return false;
+	}
+
+	var expression =
+		/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	var regex = new RegExp(expression);
+
+	if (!siteUrl.match(regex)) {
+		alert("Please use a valid URL");
+		return false;
+	}
+
+	return true;
+}
 
 function addhttp(url) {
 	if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
